f1039eeb730d7e37263f6b656e11da8f27546c70
svetla.g <svetlana.georgieva@edu1.pgzaimov.com>
2023-02-12 12:10:28 +0200

    firs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Zadrekursiq
+{
+    internal class Program
+    {
+        static int Sum(int n)
+        {
+            if (n <= 1) return 1;
+            else return n+Sum(n - 1);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int sum = Sum(n);
+            Console.WriteLine($"sum={sum}");
+        }
+    }
+}
